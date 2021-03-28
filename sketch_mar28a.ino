9dd669fd1526beb7473f5f7e04e8c51997aac42b
SunnyMate <szacun97@gmail.com>
2021-03-28 15:46:20 +0200

    version: 0.3

--- sketch_mar28a.ino
+++ sketch_mar28a.ino
@@ -1,5 +1,48 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+const char* ssid = "ASUS_2G";
+const char* password = "Sandomierska68123xp";
+
+const char* serverName = "http://192.168.0.251/test";
+
+unsigned long lastTime = 0;
+unsigned long timerDelay = 5000;
+
 void setup() {
+  Serial.begin(115200);
+  
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+  
+  Serial.println("Timer set to 5 seconds (timerDelay variable), it will take 5 seconds before publishing the first reading.");
 }
 
 void loop() {
+  if ((millis() - lastTime) > timerDelay) {
+    if(WiFi.status()== WL_CONNECTED){
+      HTTPClient http;
+      
+      http.begin(serverName);
+      
+      http.addHeader("Content-Type", "application/json");
+      int httpResponseCode = http.POST("{\"api_key\":\"tPmAT5Ab3j7F9\",\"sensor\":\"BME280\",\"value1\":\"24.25\",\"value2\":\"49.54\",\"value3\":\"1005.14\"}");
+      
+      Serial.print("HTTP Response code: ");
+      Serial.println(httpResponseCode);
+      
+      http.end();
+    }
+    else {
+      Serial.println("WiFi Disconnected");
+    }
+    lastTime = millis();
+  }
 }
